da5961a47e7707fade193a9f1bea32b4d794635e
Ann-Sofi Lenander <annsofi.len@gmail.com>
2022-01-23 20:37:49 +0300

    add className s to GameStatistics.js

--- src/components/GameStatistics.js
+++ src/components/GameStatistics.js
@@ -1,6 +1,8 @@
 import Game from "./Game"
 import { useSelector } from 'react-redux';
 
+import '../styling/game-statistics.css';
+
 
 
 function GameStatistics() {
@@ -23,7 +25,7 @@ function GameStatistics() {
                 paperCount += 1;
             }
         }
-        return [rockCount, paperCount, scissorsCount];
+        return [rockCount, scissorsCount, paperCount];
     }
 
     // count the number each move has been set for the computer
@@ -32,55 +34,75 @@ function GameStatistics() {
         let paperCount = 0;
         let scissorsCount = 0;
         for (let game of history) {
-            if (game.playerMove === 0) {
+            if (game.computerMove === 0) {
                 rockCount += 1;
-            } else if (game.playerMove === 1) {
+            } else if (game.computerMove === 1) {
                 scissorsCount += 1;
-            } else if (game.playerMove === 2) {
+            } else if (game.computerMove === 2) {
                 paperCount += 1;
             }
         }
-        return [rockCount, paperCount, scissorsCount];
+        return [rockCount, scissorsCount, paperCount];
     }
 
 
 
 
     return (
-        <div>
+        <div className="stat-parent">
             <h2>Game Statistics</h2>
-            <div>
-                <h3>User´s Wins</h3>
-                <span className='score'> <p>{nameOfPlayer.toUpperCase()}:  {Math.round(player / (player + computer + 0.000001) * 100)}% </p> </span>
-                <h3>Computer´s  Wins</h3>
-                <span className='score' ><p> COMPUTER:   {Math.round(computer / (player + computer + 0.000001) * 100)}% </p></span>
+            <div className="position-parent">
+
+                <div className="all-user">
+                    <h3>User´s Wins</h3>
+                    <span className='score'> <p>{nameOfPlayer.toUpperCase()}:  {Math.round(player / (player + computer + 0.000001) * 100)}% </p> </span>
+
+                    <div>
+                        <h3>User´s Moves</h3>
+                        <h4>Rock</h4>
+                        <p>{
+                            Math.round(moveStatistics()[0] / (player + computer + 0.000001) * 100)
+                        } %</p>
+
+                        <h4>Scissors</h4>
+                        <p>{
+                            Math.round(moveStatistics()[1] / (player + computer + 0.000001) * 100)
+                        } %</p>
+
+                        <h4>Paper</h4>
+                        <p>{
+                            Math.round(moveStatistics()[2] / (player + computer + 0.000001) * 100)
+                        } %</p>
+
+                    </div>
+                </div>
+
+
+                <div className="all-computer">
+                    <h3>Computer´s  Wins</h3>
+                    <span className='score' ><p> COMPUTER:   {Math.round(computer / (player + computer + 0.000001) * 100)}% </p></span>
+
+                    <div>
+                        <h3>Computer´s  Moves</h3>
+                        <h4>Rock</h4>
+                        <p> {Math.round(moveStatistiComputer()[0] / (player + computer + 0.000001) * 100)} %</p>
+
+                        <h4>Scissors</h4>
+                        <p> {Math.round(moveStatistiComputer()[1] / (player + computer + 0.000001) * 100)} %</p>
+
+                        <h4>Paper</h4>
+                        <p> {Math.round(moveStatistiComputer()[2] / (player + computer + 0.000001) * 100)} %</p>
+
+                    </div>
+
+                </div>
+
+
             </div>
 
-            <div>
-                <h3>User´s Moves</h3>
-                <h4>Rock</h4>
-                <p>{
-                    Math.round(moveStatistics()[0] / (player + computer + 0.000001) * 100)
-                } %</p>
-                <h4>Paper</h4>
-                <p>{
-                    Math.round(moveStatistics()[2] / (player + computer + 0.000001) * 100)
-                } %</p>
-                <h4>Scissors</h4>
-                <p>{
-                    Math.round(moveStatistics()[1] / (player + computer + 0.000001) * 100)
-                } %</p>
-            </div>
 
-            <div>
-                <h3>Computer´s  Moves</h3>
-                <h4>Rock</h4>
-                <p> {Math.round(moveStatistiComputer()[0] / (player + computer + 0.000001) * 100)} %</p>
-                <h4>Paper</h4>
-                <p> {Math.round(moveStatistiComputer()[2] / (player + computer + 0.000001) * 100)} %</p>
-                <h4>Scissors</h4>
-                <p> {Math.round(moveStatistiComputer()[1] / (player + computer + 0.000001) * 100)} %</p>
-            </div>
+
+
         </div>
     )
 }
