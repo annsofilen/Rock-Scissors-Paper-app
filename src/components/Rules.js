2a2fed3cf2e04f29444ed520a94c17e7acad4a60
Ann-Sofi Lenander <annsofi.len@gmail.com>
2022-01-23 23:57:14 +0300

    update Rules.js and readme.md

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -6,8 +6,7 @@ function Rules() {
     return (
         <div className="rules-box">
             <h2>Rules of Rock, Scissors, Paper</h2>
-            <p>Although the game has a lot of complexity to it, the rules to play it are pretty simple.
-                The game is played where players deliver hand signals that will represent the elements of the game; rock, paper and scissors. The outcome of the game is determined by 3 simple rules:
+            <p>The game Rock Paper Scissors is a widely known game that children and people have enjoyed themselves with since a young age. The two players selects from three move options: ROCK, PAPER and SCISSORS. The outcome of the game is determined by 3 simple rules:
             </p>
             <ul>
                 <li>Rock wins against scissors.</li>
@@ -15,7 +14,7 @@ function Rules() {
                 <li>Paper wins against rock.</li>
             </ul>
 
-
+            <p>Have fun!</p>
 
         </div>
     )
